1a35d7c9292c704419413af2cdb03796fb232341
Douglas <douglas.vega@unir.net>
2019-10-10 11:06:28 -0400

    Inicio

---
+++ TransHaruhiku/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TransHaruhiku.Startup))]
+namespace TransHaruhiku
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
