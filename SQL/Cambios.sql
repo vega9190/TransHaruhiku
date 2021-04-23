d2041e03ef60df469dd9ac89db3579ebd56ad2d6
Douglas Vega <douglas.vega@unir.net>
2021-04-23 17:44:56 -0400

    Cambios en archivos SQL

--- SQL/Cambios.sql
+++ SQL/Cambios.sql
@@ -1,10 +1,13 @@
 USE Gestor
 GO
-
+select * from th.Contenedores
 ALTER TABLE th.Pedidos ADD Contenedores NVARCHAR(250) NULL;
 
 ALTER TABLE th.Contenedores ALTER COLUMN Codigo VARCHAR (250) NOT NULL;
 ALTER TABLE th.Contenedores DROP COLUMN Poliza;
+
+DROP TABLE th.DetallePolizas;
+DROP TABLE th.Polizas;
 
 CREATE TABLE th.Polizas(
 IdPoliza INT PRIMARY KEY IDENTITY(1,1),
@@ -21,3 +24,32 @@ Precio DECIMAL(10,2) NOT NULL,
 IdPoliza INT NOT NULL,
 FOREIGN KEY (IdPoliza) REFERENCES th.Polizas (IdPoliza)
 )
+
+CREATE TABLE th.Empresas(
+IdEmpresa INT PRIMARY KEY IDENTITY(1,1),
+Nombre NVARCHAR(250) NOT NULL,
+Activa BIT NOT NULL
+)
+
+CREATE TABLE th.Usuarios_Empresas(
+IdUsuario INT NOT NULL,
+IdEmpresa INT NOT NULL,
+Fecha DATETIME NOT NULL,
+PRIMARY KEY(IdUsuario, IdEmpresa),
+FOREIGN KEY (IdUsuario) REFERENCES th.Usuarios (IdUsuario),
+FOREIGN KEY (IdEmpresa) REFERENCES th.Empresas (IdEmpresa)
+)
+
+ALTER TABLE th.Pedidos
+ADD IdEmpresa INT NOT NULL;
+
+ALTER TABLE th.Clientes
+ADD IdEmpresa INT NOT NULL;
+
+ALTER TABLE th.Pedidos
+ADD CONSTRAINT FK_EmpresaPedido
+FOREIGN KEY (IdEmpresa) REFERENCES th.Empresas(IdEmpresa);
+
+ALTER TABLE th.Clientes
+ADD CONSTRAINT FK_EmpresaCliente
+FOREIGN KEY (IdEmpresa) REFERENCES th.Empresas(IdEmpresa);
--- SQL/TablasNuevas.sql
+++ SQL/TablasNuevas.sql
@@ -239,6 +239,6 @@ INSERT INTO th.TiposPagos VALUES ('Multa DAV', 'Multa por DAV')
 INSERT INTO th.TiposMonedas VALUES ('Bolivianos', 'BOB')
 INSERT INTO th.TiposMonedas VALUES ('Dólar Americano', '$')
 
-select * from th.Usuarios
-update th.usuarios set IdRol = 2
-select * from th.Roles
+--select * from th.Usuarios
+--update th.usuarios set IdRol = 2
+--select * from th.Roles
